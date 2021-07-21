327d7adbbbb01356191222c54a37b36dd930e927
Cate Uch <cat.uch@yandex.ru>
2021-07-22 01:32:13 +0300

    project css

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Header />
+        <Header />
+      </header>
       <Meals />
-        </header>
     </div>
   );
 }
--- src/components/Meals/Meals.tsx
+++ src/components/Meals/Meals.tsx
@@ -1,40 +1,50 @@
 //@ts-nocheck
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import style from './Meals.module.css';
+import MealsSummary from './MealsSummary';
 
 
-function Meals () {
 
-    const [ listOfMeals, setListOfMeals] = useState([
-        {name: 'Sushi',
-        description: 'Finest fish and Veggies',
-        price: '$22.99'},
+function Meals() {
 
-        {name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: '$18.99'},
+    const [listOfMeals, setListOfMeals] = useState([
+        {
+            name: 'Sushi',
+            description: 'Finest fish and Veggies',
+            price: '$22.99'
+        },
 
-        {name: 'Schnitzel',
-        description: 'A german speciality',
-        price: '$16.99'}
-    ]
+        {
+            name: 'Green Bowl',
+            description: 'Healthy...and green...',
+            price: '$18.99'
+        },
 
-    );
+        {
+            name: 'Schnitzel',
+            description: 'A german speciality',
+            price: '$16.99'
+        }
+    ]);
+    console.log(listOfMeals)
 
     return (
-        <>
-        <div className={style.meals}>
+<>
+<MealsSummary />
+    <div className={style.mealsWindow}>
             {listOfMeals.map(item => {
-                <div>
-
-                    <span>{item.name}</span>
-                    <span>{item.description}</span>
-                    <span>{item.price}</span>
+                return (
+                    <div className={style.meals}>
+                        <span className={style.mealsName}>{item.name}</span>
+                        <span className={style.mealsDescriptione}>{item.description}</span>
+                        <span className={style.mealsPrice}>{item.price}</span>
                     </div>
+                )
             }
             )}
-        </div>
-        </>
+            </div>
+</>
+
     )
 }
 
