93595edd5bf53a6ab96553a1723c4be7ce1d1946
Cate Uch <cat.uch@yandex.ru>
2021-07-22 23:43:15 +0300

    meals components css

---
+++ src/components/Meals/MealItemForm.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import style from './MealItemForm.module.css'
+
+const MealItemForm = () => {
+    return (
+        <div className={style.form}>
+        <label htmlFor="amount">Amount </label>
+        <input type="text" value='6' name='amount'/>
+        <div>
+        <button> + Add </button>
+        </div>
+        </div>
+    )
+}
+
+export default MealItemForm;
--- src/components/Meals/Meals.tsx
+++ src/components/Meals/Meals.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import style from './Meals.module.css';
 import MealsSummary from './MealsSummary';
-
+import MealItemForm from './/MealItemForm';
 
 
 function Meals() {
@@ -29,23 +29,33 @@ function Meals() {
     console.log(listOfMeals)
 
     return (
-<>
-<MealsSummary />
-    <div className={style.mealsWindow}>
-            {listOfMeals.map(item => {
-                return (
-                    <div className={style.meals}>
-                        <span className={style.mealsName}>{item.name}</span>
-                        <span className={style.mealsDescriptione}>{item.description}</span>
-                        <span className={style.mealsPrice}>{item.price}</span>
-                    </div>
-                )
-            }
-            )}
-            </div>
-</>
+        <>
+            <MealsSummary />
+            <li className={style.mealsWindow}>
+                {listOfMeals.map(item => {
+                    return (
+                        <div className={style.mealSection}>
+                            <div className={style.mealForm}><MealItemForm /></div>
+                            <div className={style.meals}>
+                                <div className={style.mealsName}>{item.name}</div>
+                                <div className={style.mealsDescriptione}>{item.description}</div>
+                                <div className={style.mealsPrice}>{item.price}</div>
 
+                            </div>
+
+                        </div>
+                    )
+                }
+
+                )}
+
+
+
+
+            </li>
+        </>
     )
+
 }
 
 export default Meals;
